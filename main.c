8bb17cf5b0dcb5dcd14dcc76443ba281f1a97668
kdakn <109918200+kdakn@users.noreply.github.com>
2022-07-25 15:22:22 +0300

    Changing position of file operations methods

--- main.c
+++ main.c
@@ -15,26 +15,161 @@ void game1vs1();
 int check(char row1[], char row2[], char row3[]);
 int file_operations_pre(player *players); //For pre-game.
 int file_operations_post(char *winner); //For post-game.
+int file_selection_deletion(FILE *filep);
 int displayScores(char *firstname ,char *secondname);//Scan a file in order to determine if current player names are in the list.
-//void displayScores(char *firstname ,char *secondname);
 
 int main()
 {
     player *players = (player*) malloc(sizeof(player)*2); //Creates a pointer which holds player names in a bin file.
     int inp;
+    FILE *filep;
 
     printf("Welcome to TicTacToe!\n");
-    printf("Type '1' for 1 vs 1\n");
-    printf("Type '2' for 1 vs Computer\n");
+    printf("Type '1' to play 1 vs 1\n");
+    printf("Type '2' to play with Computer\n");
+//    printf("Type '3' displaying results for player pair or deleting player from file\n");
 
     scanf("%d",&inp);
     if(inp == 1)
         file_operations_pre(players);
+//    else if(inp == 3)
+//        file_selection_deletion(filep);
 
     return 0;
 }
 
+int file_operations_pre(player *players)
+{
+    char x;
+    int count = 0; //Size of trimmer_first_name.
+    int score1, score2;
+    char score_char1[5], score_char2[5];
 
+    FILE *fpwrite, *fpread;
+    fpwrite = fopen("Scoreboard.txt","a+"); //It creates a new file if it doesn't exist.
+
+    char *firstnameinp = (char*) malloc(sizeof(char)*10);
+    char *secondnameinp = (char*) malloc(sizeof(char)*100);
+    char *temp1 = (char*) malloc(sizeof(char)*20); //Hold keyboard input first name.
+    char *temp2 = (char*) malloc(sizeof(char)*20); //Hold keyboard input second name.
+
+    printf("Enter a name for Player 1: ");
+    scanf("%s",firstnameinp);
+
+    printf("Enter a name for Player 2: ");
+    scanf("%s",secondnameinp);
+
+    char *trimmer_first_name = (char*) malloc(sizeof(char)*100);
+    for(int i = 0; i<strlen(firstnameinp); i++){
+
+        if(firstnameinp[i] != ' '){
+            trimmer_first_name[count++] = firstnameinp[i];
+        }
+
+    }
+    trimmer_first_name[count] = '\0';
+
+    char *trimmer_second_name = (char*) malloc(sizeof(char)*100);
+    count = 0;
+    for(int i = 0; i<strlen(secondnameinp); i++){
+
+        if(secondnameinp[i] != ' '){
+            trimmer_second_name[count++] = secondnameinp[i];
+        }
+
+    }
+    trimmer_second_name[count] = '\0';
+
+    strcpy(temp1, trimmer_first_name);
+    strcpy(temp2, trimmer_second_name);
+    int scan_file_result = displayScores(trimmer_first_name ,trimmer_second_name);
+
+    if(scan_file_result == 0){
+        players->first_score = 0;
+        players->second_score = 0;
+        strcat(temp1,"/");
+        strcat(temp1,temp2); //Now the first name and the second name are appended to each other separated by"/".
+        strcpy(players->name_vs_name,temp1);
+        strcat(temp1,":");
+
+        itoa(players->first_score,score_char1,10);
+        strcat(temp1,score_char1);
+
+
+        strcat(temp1, "-");
+        itoa(players->second_score,score_char2,10);
+        strcat(temp1,score_char2);
+
+        strcat(temp1,"\n");
+
+        fputs(temp1,fpwrite);
+        fclose(fpwrite);
+    }
+    else if (scan_file_result == 1){
+        system("cls");
+        displayScores(trimmer_first_name ,trimmer_second_name);
+    }
+
+    game1vs1();
+
+
+}
+
+int displayScores(char *firstname ,char *secondname){
+
+    char all_line[100];
+    char *read_first_name;
+    char *read_second_name;
+    char *read_score1;
+    char *read_score2;
+    read_first_name = (char*)malloc(sizeof(char)*100);
+    read_second_name = (char*)malloc(sizeof(char)*100);
+    char *status;
+    FILE *fpread;
+    fpread = fopen("Scoreboard.txt","r+");
+
+    for(status = fgets(all_line, 100, fpread); status != 0; status = fgets(all_line, 100, fpread)){
+
+        if(all_line[strlen(all_line)-1] == '\n'){
+            all_line[strlen(all_line)-1] = '\0';
+            read_first_name = strtok(all_line,"/");
+            read_second_name = strtok(NULL,":");
+            read_score1 = strtok(NULL,"-");
+            read_score2 = strtok(NULL,"\n");
+        }
+
+        if(read_first_name!= NULL && read_second_name != NULL && strcmp(strupr(firstname),strupr(read_first_name)) == 0
+           && strcmp(strupr(secondname),strupr(read_second_name)) == 0){
+
+            system("cls");
+            COORD coord;
+            coord.X = 0;
+            coord.Y = 0;
+            SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+            printf("Current score: %s-%s (%s-%s)",firstname,secondname,read_score1,read_score2);
+
+            return 1;
+       }else if (read_first_name!= NULL && read_second_name != NULL && strcmp(strupr(secondname),strupr(read_first_name)) == 0
+           && strcmp(strupr(firstname),strupr(read_second_name)) == 0){
+            system("cls");
+            COORD coord;
+            coord.X = 0;
+            coord.Y = 0;
+            SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+            printf("Current scorexd: %s-%s (%s-%s)",firstname,secondname,read_score2,read_score1);
+            return 1;
+           }
+
+
+    }
+    system("cls");
+    COORD coord;
+    coord.X = 0;
+    coord.Y = 0;
+    printf("Current score: %s-%s (%s-%s)",strupr(firstname),strupr(secondname),"0","0");
+    fclose(fpread);
+    return 0; // Means that there is no info about first player vs second player.
+}
 
 void define_grid(int x, int y, char row1[], char row2[], char row3[]){
 
@@ -264,139 +399,9 @@ int check(char row1[], char row2[], char row3[]) //Decides if the game is over.
 
 
 }
-int file_operations_pre(player *players)
-{
-    char x;
-    int count = 0; //Size of trimmer_first_name.
-    int score1, score2;
-    char score_char1[5], score_char2[5];
-
-    FILE *fpwrite, *fpread;
-    fpwrite = fopen("Scoreboard.txt","a+"); //It creates a new file if it doesn't exist.
-
-    char *firstnameinp = (char*) malloc(sizeof(char)*10);
-    char *secondnameinp = (char*) malloc(sizeof(char)*100);
-    char *temp1 = (char*) malloc(sizeof(char)*20); //Hold keyboard input first name.
-    char *temp2 = (char*) malloc(sizeof(char)*20); //Hold keyboard input second name.
-
-    printf("Enter a name for Player 1: ");
-    scanf("%s",firstnameinp);
-
-    printf("Enter a name for Player 2: ");
-    scanf("%s",secondnameinp);
-
-    char *trimmer_first_name = (char*) malloc(sizeof(char)*100);
-    for(int i = 0; i<strlen(firstnameinp); i++){
-
-        if(firstnameinp[i] != ' '){
-            trimmer_first_name[count++] = firstnameinp[i];
-        }
-
-    }
-    trimmer_first_name[count] = '\0';
-
-    char *trimmer_second_name = (char*) malloc(sizeof(char)*100);
-    count = 0;
-    for(int i = 0; i<strlen(secondnameinp); i++){
-
-        if(secondnameinp[i] != ' '){
-            trimmer_second_name[count++] = secondnameinp[i];
-        }
-
-    }
-    trimmer_second_name[count] = '\0';
-
-    strcpy(temp1, trimmer_first_name);
-    strcpy(temp2, trimmer_second_name);
-    int scan_file_result = displayScores(trimmer_first_name ,trimmer_second_name);
-
-    if(scan_file_result == 0){
-        players->first_score = 0;
-        players->second_score = 0;
-        strcat(temp1,"/");
-        strcat(temp1,temp2); //Now the first name and the second name are appended to each other separated by"/".
-        strcpy(players->name_vs_name,temp1);
-        strcat(temp1,":");
-
-        itoa(players->first_score,score_char1,10);
-        strcat(temp1,score_char1);
 
 
-        strcat(temp1, "-");
-        itoa(players->second_score,score_char2,10);
-        strcat(temp1,score_char2);
 
-        strcat(temp1,"\n");
-
-        fputs(temp1,fpwrite);
-        fclose(fpwrite);
-    }
-    else if (scan_file_result == 1){
-        system("cls");
-        displayScores(trimmer_first_name ,trimmer_second_name);
-    }
-
-    game1vs1();
-
-
-}
-
-int displayScores(char *firstname ,char *secondname){
-
-    char all_line[100];
-    char *read_first_name;
-    char *read_second_name;
-    char *read_score1;
-    char *read_score2;
-    read_first_name = (char*)malloc(sizeof(char)*100);
-    read_second_name = (char*)malloc(sizeof(char)*100);
-    char *status;
-    FILE *fpread;
-    fpread = fopen("Scoreboard.txt","r+");
-
-    for(status = fgets(all_line, 100, fpread); status != 0; status = fgets(all_line, 100, fpread)){
-
-        if(all_line[strlen(all_line)-1] == '\n'){
-            all_line[strlen(all_line)-1] = '\0';
-            read_first_name = strtok(all_line,"/");
-            read_second_name = strtok(NULL,":");
-            read_score1 = strtok(NULL,"-");
-            read_score2 = strtok(NULL,"\n");
-        }
-
-        if(read_first_name!= NULL && read_second_name != NULL && strcmp(strupr(firstname),strupr(read_first_name)) == 0
-           && strcmp(strupr(secondname),strupr(read_second_name)) == 0){
-
-            system("cls");
-            COORD coord;
-            coord.X = 0;
-            coord.Y = 0;
-            SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
-            printf("Current score: %s-%s (%s-%s)",firstname,secondname,read_score1,read_score2);
-
-            return 1;
-       }else if (read_first_name!= NULL && read_second_name != NULL && strcmp(strupr(secondname),strupr(read_first_name)) == 0
-           && strcmp(strupr(firstname),strupr(read_second_name)) == 0){
-            system("cls");
-            COORD coord;
-            coord.X = 0;
-            coord.Y = 0;
-            SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
-            printf("Current scorexd: %s-%s (%s-%s)",firstname,secondname,read_score2,read_score1);
-            return 1;
-           }
-
-
-    }
-    system("cls");
-    COORD coord;
-    coord.X = 0;
-    coord.Y = 0;
-    printf("Current score: %s-%s (%s-%s)",strupr(firstname),strupr(secondname),"0","0");
-    fclose(fpread);
-    return 0; // Means that there is no info about first player vs second player.
-
-}
 
 
 
